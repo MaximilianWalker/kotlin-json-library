d42c87a2e94b9d5a7bfd723b2abc2fd1651e8669
MaximilianWalker <diogocrava50@gmail.com>
2025-04-13 07:36:55 +0100

    Changes: Missing File

--- src/main/kotlin/core/JSONArray.kt
+++ src/main/kotlin/core/JSONArray.kt
@@ -1,6 +1,42 @@
 package core
 
-class JSONArray(override val element: MutableList<JSONElement<*>>) : JSONElement<MutableList<JSONElement<*>>> {
+class JSONArray(override val element: MutableList<JSONElement<*>>) : JSONElement<MutableList<JSONElement<*>>>, Iterable<JSONElement<*>> {
+
+    fun get(index: Int): JSONElement<*> {
+        return element[index]
+    }
+
+    fun first(): JSONElement<*> {
+        return element.first()
+    }
+
+    fun last(): JSONElement<*> {
+        return element.last()
+    }
+
+    fun isEmpty(): Boolean {
+        return element.isEmpty()
+    }
+
+    fun add(value: JSONElement<*>) {
+        element.add(value)
+    }
+
+    fun add(index: Int, value: JSONElement<*>) {
+        element.add(index, value)
+    }
+
+    fun remove(value: JSONElement<*>): Boolean {
+        return element.remove(value)
+    }
+
+    fun removeAt(index: Int): JSONElement<*> {
+        return element.removeAt(index)
+    }
+
+    override fun iterator(): Iterator<JSONElement<*>> {
+        return element.iterator()
+    }
 
     fun forEach(callback: (value: JSONElement<*>) -> Unit) {
         element.forEach { value -> callback(value) }
